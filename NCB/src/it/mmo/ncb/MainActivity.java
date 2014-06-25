c2fb7d4cb2a1c1a850ed99e148e2b6d028f87209
Marco Montanari <marco.montanari@gmail.com>
2014-06-25 23:12:46 +0200

    initial

---
+++ NCB/src/it/mmo/ncb/MainActivity.java
@@ -0,0 +1,15 @@
+package it.mmo.ncb;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
